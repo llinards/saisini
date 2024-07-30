1985af018a748d1825ba40e70e494cbc900e077d
Linards Lazdiņš <linards_lazdins@hotmail.com>
2024-07-30 15:14:40 +0300

    Add translations

---
+++ lang/lv/auth.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    
+    'failed' => 'Šie dati neatbilst mūsu ierakstiem.',
+    'password' => 'Norādītā parole ir nepareiza.',
+    'throttle' => 'Pārāk daudz mēģinājumu pieslēgties. Lūdzu, mēģiniet vēlreiz pēc :seconds sekundēm.',
+
+];
---
+++ lang/lv/passwords.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+
+    'reset' => 'Jūsu parole ir atiestatīta.',
+    'sent' => 'Mēs esam nosūtījuši paroles atiestatīšanas saiti uz jūsu e-pastu.',
+    'throttled' => 'Lūdzu, uzgaidiet pirms mēģināt vēlreiz.',
+    'token' => 'Šis paroles atiestatīšanas tokens ir nederīgs.',
+    'user' => 'Mēs nevaram atrast lietotāju ar šo e-pasta adresi.',
+
+];
