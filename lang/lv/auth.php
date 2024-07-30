9faa1aa7415801900bff9d5913963d143b26bd36
Linards Lazdiņš <linards_lazdins@hotmail.com>
2024-07-30 16:07:29 +0300

    Revert "Add translations"
    This reverts commit 1985af018a748d1825ba40e70e494cbc900e077d.

--- lang/lv/auth.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-    
-    'failed' => 'Šie dati neatbilst mūsu ierakstiem.',
-    'password' => 'Norādītā parole ir nepareiza.',
-    'throttle' => 'Pārāk daudz mēģinājumu pieslēgties. Lūdzu, mēģiniet vēlreiz pēc :seconds sekundēm.',
-
-];
