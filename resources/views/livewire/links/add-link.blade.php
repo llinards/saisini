32eaa6c13217556c6f32d4db17b806a08087e504
Linards Lazdiņš <linards_lazdins@hotmail.com>
2024-08-01 14:54:34 +0300

    Fix text wrapping

--- resources/views/livewire/links/add-link.blade.php
+++ resources/views/livewire/links/add-link.blade.php
@@ -27,7 +27,8 @@
                             <div
                                 class="mt-1 flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600"
                             >
-                                <span class="flex select-none items-center pl-3 w-auto text-gray-500">
+                                <span
+                                    class="flex select-none items-center whitespace-nowrap flex-wrap pl-3 w-auto text-gray-500">
                                     {{ URL::to('/') }}/
                                 </span>
                                 <x-text-input
