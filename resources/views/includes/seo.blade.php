a7f166adde7cc5c990eba1367adcc1b6b404d829
Linards Lazdiņš <linards_lazdins@hotmail.com>
2025-02-27 10:10:03 +0200

    Update seo.blade.php

--- resources/views/includes/seo.blade.php
+++ resources/views/includes/seo.blade.php
@@ -1,9 +1,9 @@
 <meta name="author" content="{{ config('app.name') }}">
 <meta name="locale" content="{{ app()->getLocale() }}">
 <meta name="description"
-      content="slmedia.lv - Saīsiniet saites un ģenerējiet QR kodus ātri un viegli! Mūsu rīks nodrošina saīsinātas URL saites un pielāgojamus QR kodus, kas palīdz uzlabot jūsu saišu dalīšanos un sekošanu tiešsaistē.">
+      content="saisini.lv - Saīsiniet saites un ģenerējiet QR kodus ātri un viegli! Mūsu rīks nodrošina saīsinātas URL saites un pielāgojamus QR kodus, kas palīdz uzlabot jūsu saišu dalīšanos un sekošanu tiešsaistē.">
 <meta property="og:description"
-      content="slmedia.lv - Saīsiniet saites un ģenerējiet QR kodus ātri un viegli! Mūsu rīks nodrošina saīsinātas URL saites un pielāgojamus QR kodus, kas palīdz uzlabot jūsu saišu dalīšanos un sekošanu tiešsaistē."/>
+      content="saisini.lv - Saīsiniet saites un ģenerējiet QR kodus ātri un viegli! Mūsu rīks nodrošina saīsinātas URL saites un pielāgojamus QR kodus, kas palīdz uzlabot jūsu saišu dalīšanos un sekošanu tiešsaistē."/>
 <meta property="og:url" content=" {{Request::url()}}"/>
 <meta property="og:type" content="website"/>
 <meta property="og:title" content="{{ isset($title) ? $title . ' | ' . config('app.name') : config('app.name')}}"/>
@@ -13,7 +13,7 @@
     {
       "@context": "https://schema.org",
       "@type": "WebApplication",
-      "name": "saisini.lv - Garso URL saīsinātājs un QR kodu Ģenerators",
+      "name": "saisini.lv - Garo URL saīsinātājs un QR kodu Ģenerators",
       "url": "https://saisini.lv",
       "description": "Saīsiniet saites un ģenerējiet QR kodus ātri un viegli! Mūsu rīks nodrošina saīsinātas URL saites un pielāgojamus QR kodus, kas palīdz uzlabot jūsu saišu dalīšanos un sekošanu tiešsaistē.",
       "creator": {
